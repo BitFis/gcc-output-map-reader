41a4c7f02082595ea473b5708bf16a8cdae33d48
Lucien Zuercher <lucien@zuercher.io>
2021-04-09 18:48:37 +0000

    chore: add lint and formatter

--- src/parser/__tests__/parseExample.tsx
+++ src/parser/__tests__/parseExample.tsx
@@ -1,4 +1,4 @@
-test('test parse example file to object', () => {
-    // WIP: Will be used to test the component
-    expect(true).toBeTruthy();
+test("test parse example file to object", () => {
+  // WIP: Will be used to test the component
+  expect(true).toBeTruthy();
 });
