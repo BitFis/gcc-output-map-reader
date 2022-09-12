e18b0c01e84ea7c5524cc8be2f2528f45244d593
Lucien Zuercher <lucien.zuercher@wapice.com>
2022-09-12 13:29:31 +0300

    fix(utils): Fix timer print output

--- src/utils/Timer.tsx
+++ src/utils/Timer.tsx
@@ -9,6 +9,10 @@ class Timer {
     this.start = Date.now();
   }
 
+  Format(): string {
+    return Timer.Format(this.ElapsedMs());
+  }
+
   static async SleepMs(ms: number): Promise<void> {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
@@ -18,14 +22,14 @@ class Timer {
   }
 
   static Format(elapsedMs: number): string {
-    const ms = elapsedMs % 60;
+    const ms = elapsedMs % 1000;
     elapsedMs -= ms;
-    const sec = Math.floor((elapsedMs / 60) % 60);
-    elapsedMs -= sec * 60;
-    const min = Math.floor((elapsedMs / 60 / 60) % 60);
-    elapsedMs -= min * 60 * 60;
-    const hours = Math.floor(elapsedMs / 60 / 60 / 60);
-    elapsedMs -= hours * 60 * 60 * 60;
+    const sec = Math.floor((elapsedMs / 1000) % 60);
+    elapsedMs -= sec * 1000;
+    const min = Math.floor((elapsedMs / 60 / 1000) % 60);
+    elapsedMs -= min * 60 * 1000;
+    const hours = Math.floor(elapsedMs / 60 / 60 / 1000);
+    elapsedMs -= hours * 60 * 60 * 1000;
 
     const res = [
       Timer.str(hours, "h"),
